4f07fb35d3229f645d278b76f9cc73dd162d0e4e
Hardikk Kamboj <64458111+hady68@users.noreply.github.com>
2023-06-30 22:38:04 +0530

    Create Sum_without_highest_lowest_number.js

---
+++ 8kyu/Sum_without_highest_lowest_number.js
@@ -0,0 +1,22 @@
+//  Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+// Mind the input validation.
+
+//Solution
+function sumArray(array) {
+  if (array == null) {
+    return 0;
+  } else if (array.length < 2) {
+    return 0;
+  } else {
+    array = array.sort(function(a,b) {return a - b;});
+    var total = 0;
+    for (var i = 1; i < array.length - 1; i++) {
+      total += array[i];
+    }
+    return total;
+  }
+}
+
+// OP
+sumArray = a => a ? a.sort((x, y) => x - y).slice(1, -1).reduce((s, e) => s + e, 0) : 0
