c5ceb60377ad543028145884a1729f3fb5a19363
Hardikk Kamboj <64458111+hady68@users.noreply.github.com>
2023-07-02 14:28:00 +0530

    Create Does_mynumber_look_big.js

---
+++ 6kyu/Does_mynumber_look_big.js
@@ -0,0 +1,43 @@
+// question 
+// A Narcissistic Number (or Armstrong Number) is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
+
+// For example, take 153 (3 digits), which is narcissistic:
+
+//     1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+// and 1652 (4 digits), which isn't:
+
+//     1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+// The Challenge:
+
+// Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10.
+
+// Solution 
+function narcissistic(value) {
+  
+  let numDigits = String(value).length;
+  
+  let sum = 0;
+  
+  let temp = value;
+  
+  while( temp > 0){
+    
+    let digit = temp%10;
+    sum += Math.pow(digit, numDigits);
+    temp = Math.floor(temp / 10);
+    
+  }
+  return sum === value;
+
+
+}
+
+
+// Op
+function narcissistic(value) {
+  return value.toString()
+              .split('')
+              .map( (x,i,arr) => x ** arr.length)
+              .reduce( (a,b)=> +a + +b) 
+               === value
+}
