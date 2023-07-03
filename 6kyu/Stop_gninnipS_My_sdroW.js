226256f8b81bb2507e27ef83b8729912a12e2e38
Hardikk Kamboj <64458111+hady68@users.noreply.github.com>
2023-07-03 15:59:18 +0530

    Update Stop_gninnipS_My_sdroW.js

--- 6kyu/Stop_gninnipS_My_sdroW.js
+++ 6kyu/Stop_gninnipS_My_sdroW.js
@@ -19,3 +19,9 @@ function spinWords(string) {
   }
   return words.join(" ");
 }
+// Op 
+function spinWords(words){
+  return words.split(' ').map(function (word) {
+    return (word.length > 4) ? word.split('').reverse().join('') : word;
+  }).join(' ');
+}
