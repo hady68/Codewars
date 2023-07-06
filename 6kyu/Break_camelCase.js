5ca89933cf9a86dc102e14b4826d63493a9190c9
Hardikk Kamboj <64458111+hady68@users.noreply.github.com>
2023-07-07 00:10:50 +0530

    Update Break_camelCase.js

--- 6kyu/Break_camelCase.js
+++ 6kyu/Break_camelCase.js
@@ -6,3 +6,27 @@
 // ""             =>  ""
 
 //Solution
+function solution(string) {
+  let result = "";
+  
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === string[i].toUpperCase()) {
+      result += " " + string[i];
+    } else {
+      result += string[i];
+    }
+  }
+  
+  return result;
+}
+
+//OP
+function solution(string) {
+  string = string.split('').map(function (el) {
+    if (el === el.toUpperCase()) {
+      el = ' ' + el
+    }
+    return el
+  })
+  return string.join('')
+}
